3ec0fea99b1f82bfa864b3d8876e9d1a850a161b
Stephen Levy <slevy3@live.spcollege.edu>
2018-07-30 10:55:42 -0400

    trying to resolve the error with missing modules

--- src/App.js
+++ src/App.js
@@ -26,13 +26,6 @@ const list =
 },
 ];
 
-const more = [
-  {
-    title: 'Hello',
-    ObjectID: 3,
-  }
-]
-
 function isSearched(searchTerm){
   return function(item){
     return item.title.toLowerCase().includes(searchTerm.toLowerCase())
@@ -66,42 +59,68 @@ onDismiss(id){
 
 }
 
-  render()
-  {
-    return (
-      <div className="App">
-      <form>
-      <input type="text"
-      onChange={this.onSearchChange}/>
-      </form>
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
+render()
+{
+  const {searchTerm, list} = this.state;
+  return (
+    <div className="App">
+    <Search
+    value= {searchTerm}
+    onChange = {this.onSearchChange}/>
+    <Table
+    list ={list}
+    pattern = {searchTerm}
+    onDismiss = {this.onDismiss} />
+    </div>
 
-            <ul>
-          <li key="item.objectID">
+  );
+}
+
+
+}
+
+  class Search extends Component {
+    render() {
+      const {value, onChange} = this.props;
+      return (
+        <form>
+        <input
+        type="text"
+        value = {value}
+        onChange = {onChange}
+        />
+        </form>
+      );
+    }
+  }
+
+  class Table extends Component {
+    render() {
+      const {list, pattern, onDismiss} = this.props;
+      return (
+        <div>
+        {list.filter(isSearched(pattern)).map(item =>
+          <div key={item.objectID}>
           <span>
-          <a href={item.url}> {item.title} </a>
+          <a href={item.url}> {item.title}</a>
           </span>
           <span>{item.author} </span>
           <span>{item.num_comments} </span>
           <span>{item.points} </span>
           <span>
           <button
-          onClick={() => this.onDismiss(item.objectID)}
+          onClick={() => onDismiss(item.objectID)}
           type="button"
           >
           Dismiss
           </button>
           </span>
-          </li>
-          </ul>
-
-
+          </div>
         )}
         </div>
-        )}
 
-
+      );
+    }
   }
-
 
 export default App;
